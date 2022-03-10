6f157dab76a4de6a01cef2abbe4747d8e9ab8499
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-03-10 22:06:03 +0600

    Products list Created

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,6 +7,24 @@
     const messageDisplay = document.querySelector('#messageDisplay');
     const productsCollection = document.querySelector('#productsCollection');
 
+    let products = [
+        {
+            id: 1,
+            name: 'Potato',
+            price: 50
+        },
+        {
+            id: 2,
+            name: 'Soyabin Oil',
+            price: 500
+        },
+        {
+            id: 3,
+            name: 'Watermelon',
+            price: 789
+        }
+    ];
+
     productEntryButton.addEventListener('click', e => {
         e.preventDefault();
         // Receiving Inputs
@@ -18,9 +36,22 @@
         if(error){
             showErrorMessage('Invalid Input');
         }else{
+            products.push({
+                id: products.length+1,
+                name: productNameEntry,
+                price: productPriceEntry
+            });
+            console.log(products);
             resetInput();
             addItemToFrontend(productNameEntry, productPriceEntry);
             showSuccessMessage('Success !!!');
+        }
+    });
+
+    productsCollection.addEventListener('click',e=>{
+        e.preventDefault();
+        if(e.target.classList.contains('deleteBtn')){
+            console.log('delete button selected');
         }
     });
 
@@ -65,10 +96,11 @@
 
     function addItemToFrontend(name, price){
         const item = `
-            <tr>
+            <tr id="item-1">
                 <td class="text-center">${name}</td>
                 <td class="text-end">BDT. ${price}</td>
-                <td class="text-center"><button type="button" style="background: none; border: 0; padding:0"><i class="fa fa-trash text-danger"></i></button></td>
+                <td class="text-center"><button type="button" style="background: none; border: 0; padding:0">
+                <i class="fa fa-trash text-danger deleteBtn"></i></button></td>
             </tr>
         `;
         productsCollection.insertAdjacentHTML('beforeend',item);
